eec6aa73d52759839b9c55be9d02d91a1fce1dc7
Marian Růžek <marian.r@email.cz>
2017-02-07 01:34:03 +0100

    replacing sass.logError for tray bar BUG notification and detailed bug info in console
    This bringing new dependency 'gulp-notify', but its cool for having it. (TODO similar with js/html bug handling)

--- gulp/sass.js
+++ gulp/sass.js
@@ -5,6 +5,8 @@ var gulp = require('gulp'),
   concat = require('gulp-concat'),
   connect = require('gulp-connect'),
   sass = require('gulp-sass'),
+  gutil = require('gulp-util'),
+  notify = require('gulp-notify'),    
   sourcemaps = require('gulp-sourcemaps');
 
 var sassOptions = {
@@ -15,8 +17,26 @@ var sassOptions = {
 // Compile SASS with sourcemaps + livereload.
 gulp.task('sass', function () {
   gulp.src(global.paths.sass)
-    .pipe(sourcemaps.init())
-    .pipe(sass(sassOptions).on('error', sass.logError))
+    .pipe(sourcemaps.init())    
+    .pipe(sass(sassOptions)
+      .on('error', function (err) {
+        var lineNumber = (err.line) ? 'Line: ' + err.line + ',' : '';
+        var columnNumber = (err.column) ? ' Column: ' + err.column + '   \n' : '';
+        var pluginName = (err.plugin) ? err.plugin : '';
+        var bugMessage = (err.messageFormatted) ? err.messageFormatted : '';
+        var buggedFilePath = (err.relativePath) ? err.relativePath : '';
+        var buggedFile = err.file.replace(/^.*[\\\/]/, '');
+        var bugLocation = 'in ' + buggedFile + ' on ' +
+          lineNumber + columnNumber + 'Location: ' + buggedFilePath + '\t\t';
+        notify.onError({
+          title: 'ERROR when running ' + '[' + pluginName + ']',
+          message: bugLocation,
+          sound: 'Beep'
+        })(err);
+        gutil.log(gutil.colors.bgRed('ERROR: ', bugMessage));
+        this.emit('end');
+      })
+     )
     .pipe(concat('app.css'))
     .pipe(autoprefixer())
     .pipe(sourcemaps.write('.'))
